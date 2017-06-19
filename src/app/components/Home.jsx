cc3cff2fffae8f1c1a89b5421b5428056f217b1e
Rubiksron <rubiksron@gmail.com>
2017-06-19 16:55:27 -0700

    Working with multiple components. Feeling my way through this review.

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -1,11 +1,53 @@
 import React from "react";
 
 export class Home extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      age: props.initialAge,
+      status: 0
+  };
+  setTimeout(() => {
+    this.setState({
+      status: 1
+    });
+  }, 3000);
+}
+
+  onMakeOlder() {
+    this.setState({
+      age: this.state.age + 3
+    });
+  }
   render() {
     return(
       <div>
         <p>In the Home Component!!</p>
+        <p>My name is {this.props.name}, my age is {this.state.age}</p>
+        <p>Status: {this.state.status}</p>
+        <hr/>
+        <button onClick={this.onMakeOlder.bind(this)} className="btn btn-primary">Make me 3 yrs  older!</button>
+        <hr/>
+        <button onClick={this.props.greet} className="btn btn-primary">Greet</button>
       </div>
     )
   }
 }
+
+Home.propTypes = {
+  name: React.PropTypes.string,
+  age: React.PropTypes.number,
+  greet: React.PropTypes.func
+};
+
+
+
+
+
+
+{/* <div>
+  <h4>Hobbies</h4>
+  <ul>
+  {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
+</ul>
+</div> */}
--- src/app/index.js
+++ src/app/index.js
@@ -5,17 +5,21 @@ import { Header } from "./components/Header.jsx";
 import { Home } from "./components/Home.jsx";
 
 class App extends React.Component {
+  onGreet() {
+    alert("Hello, Lauren!!")
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-xs-10 col-xs-offset-1">
-            <Header/>
+            <Header homeLink="Home"/>
           </div>
         </div>
         <div className="row">
           <div className="col-xs-10 col-xs-offset-1">
-            <Home/>
+            <Home name={"Ron"} initialAge={38} greet={this.onGreet}/>
           </div>
         </div>
       </div>
